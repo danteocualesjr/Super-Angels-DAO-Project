1f36ab8f1a1a314f86ce3d7253cd516dc702943e
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-03 13:03:55 +0800

    Used connectWallet hook from ThirdWeb

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import { useEffect, useMemo, useState } from "React";
 import { useWeb3 } from "@3rdweb/hooks";
 
 const App = () => {
+  // Use the connectWallet hook ThirdWeb gives us
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("👋 Address:", address);
+
   return (
     <div className="landing">
       <h1>Welcome to SuperAngels DAO 😇</h1>
