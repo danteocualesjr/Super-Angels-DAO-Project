724dcd748387ab3ea8faacb099290b45dc4b498a
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-03 12:47:27 +0800

    Imported ThirdWeb

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.jsx";
 
+// Import ThirdWeb
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks';
+
 // Render the App component to the DOM
 ReactDOM.render(
   <React.StrictMode>
