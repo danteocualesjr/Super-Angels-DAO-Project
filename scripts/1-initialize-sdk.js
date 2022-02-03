898ccf8209874af5cd559abff27e5cb289c442f4
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-03 13:46:43 +0800

    Imported dotenv

--- scripts/1-initialize-sdk.js
+++ scripts/1-initialize-sdk.js
@@ -1,2 +1,7 @@
-import { ThirdwebSDK } from '@3rdweb/sdk';
-import ethers from 'ethers';
+import { ThirdwebSDK } from "@3rdweb/sdk";
+import ethers from "ethers";
+
+//Importing and configuring our .env file that we use to securely store our environment variables
+import dotenv from "dotenv";
+dotenv.config();
+
