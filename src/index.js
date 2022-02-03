25a509a3383d14107ebe4626e8eec3dbfac81ef6
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-03 12:55:18 +0800

    Wrapped app with ThirdwebWeb3Provider

--- src/index.js
+++ src/index.js
@@ -20,7 +20,12 @@ const connectors = {
 // Render the App component to the DOM
 ReactDOM.render(
   <React.StrictMode>
+    <ThirdwebWeb3Provider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
     <App />
+    </ThirdwebWeb3Provider>    
   </React.StrictMode>,
   document.getElementById("root")
 );
