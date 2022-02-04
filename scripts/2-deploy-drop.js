d635e5c814724fb3dd85d578e4c85f9f022867b9
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-04 11:28:57 +0800

    Updated address in line 5

--- scripts/2-deploy-drop.js
+++ scripts/2-deploy-drop.js
@@ -2,5 +2,5 @@ import { ethers } from "ethers";
 import sdk from "./1-initialize-sdk.js";
 import { readFileSync } from "fs";
 
-const app = sdk.getAppModule("https://eth-rinkeby.alchemyapi.io/v2/1dWf8nhPmMMtB2SZ8l1gh-3yeloafe_S");
+const app = sdk.getAppModule("0xF1013c2c258EA9756a8ee1B64e9a5c6313C3f4f1");
 
