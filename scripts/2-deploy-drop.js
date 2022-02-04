7e83b57bf1b11b7ad993e20d3c2b06efda4c7bfd
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-04 11:33:41 +0800

    Added code, from line 7 onwards

--- scripts/2-deploy-drop.js
+++ scripts/2-deploy-drop.js
@@ -4,3 +4,16 @@ import { readFileSync } from "fs";
 
 const app = sdk.getAppModule("0xF1013c2c258EA9756a8ee1B64e9a5c6313C3f4f1");
 
+(async () => {
+    try {
+        const bundleDropModule = await app.deployBundleDropModule({
+            // The collection's name, ex. CryptoPunks
+            name: "SuperAngelsDAO Membership",
+            // A description for the collection
+            description: "A DAO for members of SuperAngelsDAO",
+            // The image for the collection that will show up on OpenSea.
+            image: readFileSync(""),
+
+        })
+    }
+})
