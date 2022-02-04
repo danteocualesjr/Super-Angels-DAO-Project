edd254a99b0ee5feeb3822d68ee7125895dfe48d
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-04 11:27:07 +0800

    Added app

--- scripts/2-deploy-drop.js
+++ scripts/2-deploy-drop.js
@@ -1,3 +1,6 @@
 import { ethers } from "ethers";
 import sdk from "./1-initialize-sdk.js";
 import { readFileSync } from "fs";
+
+const app = sdk.getAppModule("https://eth-rinkeby.alchemyapi.io/v2/1dWf8nhPmMMtB2SZ8l1gh-3yeloafe_S");
+
