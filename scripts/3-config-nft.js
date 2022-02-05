8ae5367048ab2898b35c1d2badf0d3b557dbb190
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-05 16:30:21 +0800

    Added archangel image

--- scripts/3-config-nft.js
+++ scripts/3-config-nft.js
@@ -4,3 +4,15 @@ import { readFileSync } from "fs";
 const bundleDrop = sdk.getBundleDropModule(
     "0x267E587a9e541bF967DDB38913C267FBfb5e80C5",
 );
+
+(async () => {
+    try {
+        await bundleDrop.createBatch([
+            {
+                name: "Archangels",
+                description: "This NFT will give you access to SuperAngelsDAO",
+                image: readFileSync("scripts/assets/Archangel.jpg"),
+            }
+        ])
+    }
+})
