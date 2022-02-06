8b3e0075ebf1fd3dc91e79fe5bc7ec31c02b51cd
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-06 08:31:32 +0800

    Added code for async

--- scripts/4-set-claim-condition.js
+++ scripts/4-set-claim-condition.js
@@ -4,3 +4,20 @@ const bundleDrop = sdk.getBundleDropModule(
     "0x267E587a9e541bF967DDB38913C267FBfb5e80C5",
 );
 
+(async () => {
+    try {
+        const claimConditionFactory = bundleDrop.getClaimConditionFactory();
+        // Specify conditions
+        claimConditionFactory.newClaimPhase({
+            startTime: new Date(),
+            maxQuantity: 50_000,
+            maxQuantityPerTransaction: 1,
+        });
+
+        await bundleDrop.setClaimCondition(0, claimConditionFactory);
+        console.log("✅ Successfully set claim condition at bundle drop:", bundleDrop.address);
+    } catch (error) {
+      console.error("Failed to set claim condition", error);
+    }
+})()
+
