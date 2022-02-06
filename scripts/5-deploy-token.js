bd6363e15ff8ff7545dfe24aa12f3153065248f8
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-06 11:12:43 +0800

    Corrected typo in line 4

--- scripts/5-deploy-token.js
+++ scripts/5-deploy-token.js
@@ -1,7 +1,7 @@
 import sdk from "./1-initialize-sdk.js";
 
 // In order to deploy the new contract we need our old friend the app module again
-const app = sdk.getAppModule("0xF1013c2c258EA9756a8ee1B64e9a5c6313C3f4f1"),
+const app = sdk.getAppModule("0xF1013c2c258EA9756a8ee1B64e9a5c6313C3f4f1");
 
 (async () => {
     try {
