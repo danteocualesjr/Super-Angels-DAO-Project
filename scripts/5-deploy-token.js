3c034db8f2d78f169e4a3a0ee5e1943ed61c6a88
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-06 10:59:18 +0800

    Added app variable

--- scripts/5-deploy-token.js
+++ scripts/5-deploy-token.js
@@ -1,2 +1,5 @@
 import sdk from "./1-initialize-sdk.js";
 
+// In order to deploy the new contract we need our old friend the app module again.
+const app = sdk.getAppModule("0xF1013c2c258EA9756a8ee1B64e9a5c6313C3f4f1"),
+
