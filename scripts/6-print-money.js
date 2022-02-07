073fa55a5d3982371ae6c28efad0a01bd7c7fc83
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-07 08:42:00 +0800

    Added token module address

--- scripts/6-print-money.js
+++ scripts/6-print-money.js
@@ -1,3 +1,8 @@
 import { ethers } from "ethers";
 import sdk from "./1-initialize-sdk.js";
 
+// This is the address of our ERC-20 contract printed out in the step before
+const tokenModule = sdk.getTokenModule(
+    "0x63AdaaDF924A2fb501F2aE4424A520FbdD9Ad55f",
+);
+
