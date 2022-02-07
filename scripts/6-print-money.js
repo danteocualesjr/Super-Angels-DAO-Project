f2a08c15d1c186cce0ece7f07cb430768ea7a836
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-07 09:25:32 +0800

    Completed async code

--- scripts/6-print-money.js
+++ scripts/6-print-money.js
@@ -13,5 +13,17 @@ const tokenModule = sdk.getTokenModule(
         // We use the util function from "ethers" to convert the amount
         // to have 18 decimals (which is the standard for ERC20 tokens)
         const amountWith18Decimals = ethers.utils.parseUnits(amount.toString(), 18);
+        // Interact with your deployed ERC-20 contract and mint the tokens!
+        await tokenModule.mint(amountWith18Decimals);
+        const totalSupply = await tokenModule.totalSupply;
+
+        // Print out how many of our token's are out there now!
+        console.log(
+            "✅ There now is",
+            ethers.utils.formatUnits(totalSupply, 18),
+            "$SUPE in circulation",
+        );
+    } catch (error) {
+        console.error("Failed to print money", error);
     }
 })();
