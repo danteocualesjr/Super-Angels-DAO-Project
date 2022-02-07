e9c2a4a481e8f58a1ffd8dc468d9fc27d3bd6794
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-07 08:45:25 +0800

    Set max amount

--- scripts/6-print-money.js
+++ scripts/6-print-money.js
@@ -6,3 +6,12 @@ const tokenModule = sdk.getTokenModule(
     "0x63AdaaDF924A2fb501F2aE4424A520FbdD9Ad55f",
 );
 
+(async () => {
+    try {
+        // What's the max supply you want to set? 1,000,000 is a nice number!
+        const amount = 1_000_000;
+        // We use the util function from "ethers" to convert the amount
+        // to have 18 decimals (which is the standard for ERC20 tokens)
+        const amountWith18Decimals = ethers.utils.parseUnits(amount.toString(), 18);
+    }
+})();
