ca849ff39921ba5961d015d1667c6abd38649aeb
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-07 10:11:40 +0800

    Added bundle drop module address

--- scripts/7-airdrop-token.js
+++ scripts/7-airdrop-token.js
@@ -1,3 +1,8 @@
 import { ethers } from "ethers";
 import sdk from "./1-initialize-sdk.js";
 
+// This is the address to our ERC-1155 membership NFT contract
+const bundleDropModule = sdk.getBundleDropModule(
+    "0x267E587a9e541bF967DDB38913C267FBfb5e80C5",
+);
+
