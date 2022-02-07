808d359f5a2fac092cc9c3e6515db76c5920bf28
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-07 10:13:39 +0800

    Added token module address

--- scripts/7-airdrop-token.js
+++ scripts/7-airdrop-token.js
@@ -6,3 +6,7 @@ const bundleDropModule = sdk.getBundleDropModule(
     "0x267E587a9e541bF967DDB38913C267FBfb5e80C5",
 );
 
+// This is the address to our ERC-20 token contract.
+const tokenModule = sdk.getTokenModule(
+    "0x63AdaaDF924A2fb501F2aE4424A520FbdD9Ad55f",
+);
