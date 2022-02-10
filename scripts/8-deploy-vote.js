b9d153c95d9bcd2952fb9ab117ce33653c50beae
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-10 12:08:40 +0800

    Updated appModule with correct address

--- scripts/8-deploy-vote.js
+++ scripts/8-deploy-vote.js
@@ -2,7 +2,7 @@ import sdk from "./1-initialize-sdk.js";
 
 // Grab the app module address
 const appModule = sdk.getAppModule(
-    "0x63AdaaDF924A2fb501F2aE4424A520FbdD9Ad55f",
+    "0xF1013c2c258EA9756a8ee1B64e9a5c6313C3f4f1",
 );
 
 (async () => {
