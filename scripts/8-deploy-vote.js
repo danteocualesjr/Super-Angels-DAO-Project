fd4153eec079491b425ede88d9520a848b37a202
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-10 12:05:59 +0800

    Added voteModule

--- scripts/8-deploy-vote.js
+++ scripts/8-deploy-vote.js
@@ -5,3 +5,11 @@ const appModule = sdk.getAppModule(
     "0x63AdaaDF924A2fb501F2aE4424A520FbdD9Ad55f",
 );
 
+(async () => {
+    const voteModule = await appModule.deployVoteModule({
+        // Give your governance contract a name
+        name: "SuperAngelsDAO Epic Proposals",
+
+        
+    })
+})();
