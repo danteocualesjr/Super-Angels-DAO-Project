df4c0f3777e2dc4a8ecae33753ab856937fe276a
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-13 07:03:35 +0800

    Created tokenModule and added address for token module smart contract

--- scripts/9-setup-vote.js
+++ scripts/9-setup-vote.js
@@ -3,6 +3,11 @@ import sdk from "./1-initialize-sdk.js";
 
 // This is our governance smart contract
 const voteModule = sdk.getVoteModule(
-    "0x622533b911038B8AC349aB9fE0450cC492437D4C"
+    "0x622533b911038B8AC349aB9fE0450cC492437D4C",
+);
+
+// This is our ERC-20 contract
+const tokenModule = sdk.getTokenModule(
+    "0x63AdaaDF924A2fb501F2aE4424A520FbdD9Ad55f",
 );
 
