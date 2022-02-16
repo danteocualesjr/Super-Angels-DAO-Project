ea751f16b9ac84be882de06ad183824c4e80cf27
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-16 15:07:50 +0800

    Added tokenModule

--- scripts/10-create-vote-proposals.js
+++ scripts/10-create-vote-proposals.js
@@ -6,3 +6,7 @@ const voteModule = sdk.getVoteModule(
     "0x622533b911038B8AC349aB9fE0450cC492437D4C",
 );
 
+// Our ERC-20 contract
+const tokenModule = sdk.getTokenModule(
+    "0x63AdaaDF924A2fb501F2aE4424A520FbdD9Ad55f",
+)
