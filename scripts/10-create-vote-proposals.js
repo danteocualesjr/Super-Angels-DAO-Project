29e0851b7d0b5e7613cf8e7ca016ed01dcfae09d
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-16 15:06:17 +0800

    Added voteModule

--- scripts/10-create-vote-proposals.js
+++ scripts/10-create-vote-proposals.js
@@ -1,2 +1,8 @@
 import { ethers } from "ethers";
 import sdk from "./1-initialize-sdk.js";
+
+// Our voting contract
+const voteModule = sdk.getVoteModule(
+    "0x622533b911038B8AC349aB9fE0450cC492437D4C",
+);
+
