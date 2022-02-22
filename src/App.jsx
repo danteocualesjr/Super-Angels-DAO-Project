b5cb4eba4756e5ff7a9f5c41fc343ea3b2bfaa5d
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-22 14:57:12 +0800

    Added setHasVoted

--- src/App.jsx
+++ src/App.jsx
@@ -56,6 +56,10 @@ if (!proposals.length) {
 // Check if the user has already voted on the first proposal
 try {
   const hasVoted = await voteModule.hasVoted(proposals[0].proposalId, address);  
+  setHasVoted(hasVoted);
+
+
+  
 }
 
 
