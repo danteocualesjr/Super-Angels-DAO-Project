9ed8d3d72399c059b6f7a939e01af992eeb6c396
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-27 06:48:13 +0800

    Created code for revoking roles

--- scripts/11-revoke-roles.js
+++ scripts/11-revoke-roles.js
@@ -4,6 +4,23 @@ const tokenModule = sdk.getTokenModule(
     "0x63AdaaDF924A2fb501F2aE4424A520FbdD9Ad55f",
 );
 
-(async ()=> {
-    
+(async () => {
+    try {
+        // Log the current roles
+        console.log(
+            "👀 Roles that exist right now:",
+            await tokenModule.getAllRoleMembers()
+        );
+
+        // Revoke all the superpowers your wallet had over the ERC-20 contract
+        await tokenModule.revokeAllRolesFromAddress(process.env.WALLET_ADDRESS);
+        console.log(
+            "🎉 Roles after revoking ourselves:",
+            await tokenModule.getAllRoleMembers()
+        );
+
+        console.log("✅ Successfully revoked our superpowers from the ERC-20 contract");
+    } catch (error) {
+        console.error("Failed to revoke ourselves from the DAO treasury", error)
+    }
 })();
