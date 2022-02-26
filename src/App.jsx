7ca5a07c0f12c55eca4d7c2a646c86d3a407c10a
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-27 06:21:33 +0800

    Added a useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -133,6 +133,24 @@ const [proposals, setProposals] = useState([]);
 const [isVoting, setIsVoting] = useState(false);
 const [hasVoted, setHasVoted] = useState(false);
 
+// Retreive all our existing proposals from the contract.
+useEffect(() => {
+  if (!hasClaimedNFT) {
+    return;
+  }
+  // A simple call to voteModule.getAll() to grab the proposals.
+  voteModule
+    .getAll()
+    .then((proposals) => {
+      // Set state!
+      setProposals(proposals);
+      console.log("🌈 Proposals:", proposals);
+    })
+    .catch((err) => {
+      console.error("failed to get proposals", err);
+    });
+}, [hasClaimedNFT]);
+
 // Retrieve all our existing proposals from the contract
 useEffect( async () => {
   if (!hasClaimedNFT) {
