50469c05926bea35a77a86194af8471878f59bd9
Dante O. Cuales <danteocualesjr@gmail.com>
2022-10-21 12:51:00 +0800

    Refactored code

--- scripts/2-deploy-drop.js
+++ scripts/2-deploy-drop.js
@@ -1,10 +1,12 @@
 import { ethers } from "ethers";
+
 import sdk from "./1-initialize-sdk.js";
 import { readFileSync } from "fs";
 
 const app = sdk.getAppModule("0xF1013c2c258EA9756a8ee1B64e9a5c6313C3f4f1");
 
 (async () => {
+    
     try {
         const bundleDropModule = await app.deployBundleDropModule({
             // The collection's name, ex. CryptoPunks
