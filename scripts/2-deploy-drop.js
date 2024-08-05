485b5559a1e38f2a1286cea06f27cb79d915372a
Dante O. Cuales <danteocualesjr@gmail.com>
2024-08-05 10:05:21 +0800

    Updated contract

--- scripts/2-deploy-drop.js
+++ scripts/2-deploy-drop.js
@@ -29,6 +29,7 @@ const app = sdk.getAppModule("0xF1013c2c258EA9756a8ee1B64e9a5c6313C3f4f1");
             // you can set this to your own wallet address if you want to charge for the drop.
             
             primarySaleRecipientAddress: ethers.constants.AddressZero,
+            
         });
 
         console.log(
