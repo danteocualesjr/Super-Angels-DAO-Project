36b51546d96e2ef7271f619a17d8e96df650d8fa
Dante O. Cuales <danteocualesjr@gmail.com>
2024-08-08 11:28:02 +0800

    Updated contract

--- scripts/2-deploy-drop.js
+++ scripts/2-deploy-drop.js
@@ -47,8 +47,9 @@ const app = sdk.getAppModule("0xF1013c2c258EA9756a8ee1B64e9a5c6313C3f4f1");
             await bundleDropModule.getMetadata(),
             
         );
-        
+
     } catch (error) {
+        
         console.log("Failed to deploy bundleDrop module", error);
     }
 })()
