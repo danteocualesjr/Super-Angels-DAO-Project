e519e2fc2ed1d8fe33bb524e03c3610924d925c8
Dante O. Cuales <danteocualesjr@gmail.com>
2024-08-08 11:28:37 +0800

    Updated contract

--- scripts/2-deploy-drop.js
+++ scripts/2-deploy-drop.js
@@ -1,6 +1,5 @@
 import { ethers } from "ethers";
 import sdk from "./1-initialize-sdk.js";
-
 import { readFileSync } from "fs";
 
 const app = sdk.getAppModule("0xF1013c2c258EA9756a8ee1B64e9a5c6313C3f4f1");
