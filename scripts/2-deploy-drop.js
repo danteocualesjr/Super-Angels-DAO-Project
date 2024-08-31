517390f191e27f8d3a050825a2e88880c3eb6031
Dante O. Cuales <danteocualesjr@gmail.com>
2024-08-31 17:54:43 +0800

    Updated contract

--- scripts/2-deploy-drop.js
+++ scripts/2-deploy-drop.js
@@ -7,6 +7,7 @@ import { readFileSync } from "fs";
 const app = sdk.getAppModule("0xF1013c2c258EA9756a8ee1B64e9a5c6313C3f4f1");
 
 (async () => {
+    
     try {
         const bundleDropModule = await app.deployBundleDropModule({
             // The collection's name, ex. CryptoPunks
