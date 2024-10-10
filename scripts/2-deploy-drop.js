f8d64755c7d95b53d4c7a675a6101657bee777d2
Dante O. Cuales <danteocualesjr@gmail.com>
2024-10-11 04:11:53 +0800

    Refactored code

--- scripts/2-deploy-drop.js
+++ scripts/2-deploy-drop.js
@@ -13,10 +13,11 @@ const app = sdk.getAppModule("0xF1013c2c258EA9756a8ee1B64e9a5c6313C3f4f1");
         const bundleDropModule = await app.deployBundleDropModule({
             
             // The collection's name, ex. CryptoPunks
-            
+
             name: "SuperAngelsDAO Membership",
 
             // A description for the collection
+            
             description: "A DAO for members of SuperAngelsDAO",
             
             // The image for the collection that will show up on OpenSea.
